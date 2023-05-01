4e9d5d317a9d4626870b927b13c174cbff967cce
AndradeTH <deandradeoliveira2@gmail.com>
2023-05-01 17:28:20 -0300

    add the result

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import quizz_questions from '../../../assets/data/quizz_questions.json'
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
+
 export class QuizzComponent implements OnInit {
-  title: string = ""
 
-  questions: any
-  questionSelected: any
+  title:string = ""
 
-  answers: string[] = []
-  answersSelected: string = ""
+  questions:any
+  questionSelected:any
 
-  questionIndex: number = 0
-  questionMaxIndex: number = 0
+  answers:string[] = []
+  answerSelected:string =""
 
-  finished: boolean = false
+  questionIndex:number =0
+  questionMaxIndex:number=0
+
+  finished:boolean = false
 
   constructor() { }
 
@@ -32,20 +34,45 @@ export class QuizzComponent implements OnInit {
 
       this.questionIndex = 0
       this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
     }
+
   }
 
   playerChoose(value:string){
     this.answers.push(value)
+    this.nextStep()
+
   }
 
   async nextStep(){
     this.questionIndex+=1
 
-    if(this.questionIndex > this.questionMaxIndex) {
-      this.questionSelected = this.questions[this.questionIndex]
-  }else{
-
+    if(this.questionMaxIndex > this.questionIndex){
+        this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results ]
     }
   }
+
+  async checkResult(anwsers:string[]){
+
+    const result = anwsers.reduce((previous, current, i, arr)=>{
+        if(
+          arr.filter(item => item === previous).length >
+          arr.filter(item => item === current).length
+        ){
+          return previous
+        }else{
+          return current
+        }
+    })
+
+    return result
+  }
+
 }
